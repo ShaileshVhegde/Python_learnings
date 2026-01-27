8c6867bffac3823454566b1e93f926822c363ce4
ShaileshVHegde <shaileshhegdesvh@gmail.com>
2026-01-27 19:57:37 +0530

    done

--- finallyKeword.py
+++ finallyKeword.py
@@ -1,1 +1,14 @@
-t
+def func():
+    try:
+        i=[1,2,3,4]
+        index=int(input("Enter an index: "))
+        print(f"Element at index {index} is {i[index]}")
+        return 1
+    except :
+        print("An error occurred.")
+        return 0
+    finally:
+        print("Execution of the try-except block is complete.")
+    # print("This line is outside the try-except-finally block.")
+result = func()
+print(result)
