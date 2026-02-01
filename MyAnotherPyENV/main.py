f4cd063f915fcac80225110a813ad0130fcfe338
ShaileshVHegde <shaileshhegdesvh@gmail.com>
2026-02-01 23:26:45 +0530

    virtual environment

---
+++ MyAnotherPyENV/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+print("Pandas version:", pd.__version__)
+
+#Activate a virtual environment named MyAnotherPyENV before running this script.
+#MyAnotherPyENV/Scripts/activate (Windows)
+#source MyAnotherPyENV/bin/activate (MacOS/Linux)
+
+#to create requirements file
+#pip freeze > requirements.txt
