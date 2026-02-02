f966f03f89dfeed14f68469a31ca801b3c12d6cf
ShaileshVHegde <shaileshhegdesvh@gmail.com>
2026-02-02 22:19:36 +0530

    importing module and package

---
+++ Importing.py
@@ -0,0 +1,8 @@
+import math 
+#from math import sqrt
+#from math impot sqrt as s
+reuilt = math.sqrt(16)
+print(reuilt)  # This will output: 4.0
+
+
+print(dir(math))  # This will print all attributes and methods in the math module
