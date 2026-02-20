05a4b77e6d6c7ed4a190a541064170c08750a6cf
ShaileshVhegde <shaileshhegdesvh@gmail.com>
2026-02-20 23:14:25 +0530

    inheritence

---
+++ OOPs/inheritence.py
@@ -0,0 +1,12 @@
+#inheritence is a feature of OOPs that allows a new class to inherit the properties and methods of an existing class. The existing class is called the parent class or base class, and the new class is called the child class or derived class.
+
+class Employee:
+    def __init__(self,name ,age):
+        self.name =name
+        self.age = age
+        
+    def show(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+class Manager(Employee):
+    def showdetails(self):
+        print(f"Manager Name: {self.name}, Manager Age: {self.age}")
