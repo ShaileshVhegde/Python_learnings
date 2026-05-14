09c1f3519e0671a7ba82146c40835ee6880fa181
ShaileshVhegde <shaileshhegdesvh@gmail.com>
2026-05-15 00:26:38 +0530

    Pelindrome cheak of number

---
+++ IMP PGM/6_pelindrome_number.py
@@ -0,0 +1,12 @@
+num=int(input("enter the number"))
+reverse_num=0
+temp=num
+while temp>0:
+    last_digit=temp%10
+    reverse_num=reverse_num*10+last_digit
+    temp=temp//10
+
+if num==reverse_num:
+    print("you enterd pelindrome number")
+else:
+    print("your entered number is not pelindrome")
